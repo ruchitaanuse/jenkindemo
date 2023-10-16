5803d1dbfd831a0c0b37829838260a8b80cee752
ruchitaanuse <120621913+ruchitaanuse@users.noreply.github.com>
2023-10-16 15:44:05 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("public"));
-
-console.log("executing...");
-app.listen(3000, () => {
-  console.log("Server is up and running on port 3000");
-});
